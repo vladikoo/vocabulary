7fac3aceec1afd6971e0305f253773ef9a7dfc11
Vlad Lysiuk <vlad.lysiuk@litslink.com>
2021-11-10 11:31:27 +0200

    Setup initial app/javascript structure

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,10 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/components/app';
 
-import Rails from "@rails/ujs"
-
-Rails.start()
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <App/>,
+    document.body.appendChild(document.createElement('div'))
+  );
+});
---
+++ app/javascript/src/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hello from "./hello_react";
+
+const App = () => (
+  <>
+    <Hello name="React" />
+    <Hello name="Rails" />
+    <Hello />
+  </>
+);
+
+export default App;
